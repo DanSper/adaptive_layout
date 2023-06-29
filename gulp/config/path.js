99c14ce5c79da8e78dd2a29253c7261d8233037c
DanSper <danilsperantsev@gmail.com>
2023-06-29 21:04:52 +0300

    first commit, add gallery

---
+++ gulp/config/path.js
@@ -0,0 +1,35 @@
+	// Получаем имя папки проекта
+	import * as nodePath from 'path';
+	const rootFolder = nodePath.basename(nodePath.resolve());
+	
+	const buildFolder = `./dist`;
+	const srcFolder = `./src`;
+	
+	export const path = {
+		build: {
+			css: `${buildFolder}/css/`,
+			html: `${buildFolder}/`,
+			images: `${buildFolder}/img/`,
+			fonts: `${buildFolder}/fonts/`,
+			files: `${buildFolder}/files/`
+		},
+		src: {
+			scss: `${srcFolder}/scss/style.scss`,
+			html: `${srcFolder}/*.html`,
+			images: `${srcFolder}/asserts/img/**/*.{jpg,jpeg,png,gif}`,
+			svg: `${srcFolder}/asserts/img/**/*.svg`,
+			files: `${srcFolder}/files/**/*.*`
+			// fonts: `${srcFolder}/asserts/fonts/**/*.{woff2,woff}`,
+		},
+		watch: {
+			scss: `${srcFolder}/scss/**/*.scss`,
+			html: `${srcFolder}/**/*.html`,
+			images: `${srcFolder}/asserts/img/**/*.{jpg,jpeg,png,gif,svg}`,
+			files: `${srcFolder}/files/**/*.*`
+		},
+		clean: buildFolder,
+		buildFolder: buildFolder,
+		srcFolder: srcFolder,
+		rootFolder: rootFolder,
+		https: ``
+	}
---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+// Основной модуль
+import gulp from "gulp";
+// Импорт путей
+import { path } from "./gulp/config/path.js";
+// Импорт общих плагинов
+import { plugins } from "./gulp/config/plugins.js";
+
+// Передаем значения в глобальную переменную
+global.app = {
+	path: path,
+	gulp: gulp,
+	plugins: plugins
+}
+
+
+// Импорт задач
+import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
+import { server } from "./gulp/tasks/server.js";
+import { scss } from "./gulp/tasks/scss.js";
+import { images } from "./gulp/tasks/images.js";
+import { fontsStyle } from "./gulp/tasks/fonts.js";
+
+// Наблюдатель за изменениями в файлах
+function watcher() {
+	gulp.watch(path.watch.files, copy);
+	gulp.watch(path.watch.html, html);
+	gulp.watch(path.watch.scss, scss);
+	gulp.watch(path.watch.images, images);
+	// gulp.watch(path.watch.fontsStyle, fontsStyle);
+}
+
+// function fonts() {
+	// return src(path.src.fonts, {base: srcPath)
+// }
+
+const mainTasks = gulp.series( gulp.parallel(copy, html, scss, images));
+	
+// Построение сценариев выполнения задач
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+
+// Выполнение сценария по умолчанию
+gulp.task('default', dev);
